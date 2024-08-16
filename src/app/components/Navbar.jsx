8c674e945189c71620048e1dde9db931fe12bd0e
lefatsiane <154235500+lefatsiane@users.noreply.github.com>
2024-08-16 13:23:55 +0200

    navBar Corrections

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import NavLink from "./Navlink";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"; 
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverLay from "./MenuOverLay";
 
 const navLinks = [
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -81,7 +81,7 @@ const ProjectsSection = () => {
   };
 
   return (
-    <section>
+    <section  id="projects" >
       <div>
         <h2 className="text-center text-4xl font-bold text-white mt-4 mb-4">
           My projects
