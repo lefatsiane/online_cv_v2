9e815f4de3626ac4725620863a0d895a04752348
lefatsiane <154235500+lefatsiane@users.noreply.github.com>
2024-08-19 17:09:59 +0200

    logo's

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="footer border border-t-[#33353F] z-10 border-l-transparent border-r-transparent border-b-transparent text-white">
       <div className="container p12 flex justify-between  ">
-        <span>Logo </span>
-        <p className="text-slate-600">all rights reserved</p>
+        <span>
+          <Link href={"/"}>
+            <img
+              src="/images/logo_size_invert.jpg"
+              alt="logo"
+              className="w-12.3 h-14"
+            />
+          </Link>{" "}
+        </span>
+        <p className="text-slate-600 mt-3">all rights reserved</p>
       </div>
     </footer>
   );
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -24,8 +24,8 @@ function Navbar() {
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex flex-wrap items-center justify-between mx-auto px-8 py-2 ">
-        <Link href={"/"} className="text-5xl text-white font-semibold">
-          LOGO
+        <Link href={"/"} >
+          <img src="/images/logo_size_invert.jpg" alt="logo" className="w-12.3 h-14" />
         </Link>
 
         <div className=" mobile-menu block md:hidden ">
